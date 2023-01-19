60776c64c98607e0188a71046019c82e2547a05a
michael <michael.jones@sethhan.com>
2023-01-19 10:13:46 +0100

    minor progress

--- caesar/src/main.rs
+++ caesar/src/main.rs
@@ -1,16 +1,29 @@
 use std::io::{self, Write};
+use std::env;
 
 fn main() {
+    let key = get_key();
     let text = get_text();
+    println!("{} {}", key, text);
 }
 
 fn get_text() -> String {
-    loop {
-        let mut prompt_text: String = String::new();
-        print!("Key: ");
-        io::stdout().flush().expect("error: failed to flush stdout");
-        io::stdin()
-            .read_line(&mut prompt_text)
-            .expect("error: failed to read from stdin");
+    let mut prompt: String = String::new();
+    print!("plaintext  : ");
+    io::stdout().flush().expect("error: failed to flush stdout");
+    io::stdin()
+        .read_line(&mut prompt)
+        .expect("error: failed to read from stdin");
+    prompt.to_string()
+}
+
+fn get_key() -> String {
+    let args: Vec::<String> = env::args().collect();
+    if args.len() != 2 {
+        println!("usage: {} <cipher-key>", &args[0]);
+        std::process::exit(1);
+        } else {
+        (args[1]).to_string()
     }
+
 }
