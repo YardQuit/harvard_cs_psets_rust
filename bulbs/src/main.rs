2aa984e5157ec6518cc468c7e33df74640f55c7f
michael <michael.jones@sethhan.com>
2023-01-21 15:50:09 +0100

    updated bulbs

--- bulbs/src/main.rs
+++ bulbs/src/main.rs
@@ -2,16 +2,7 @@ use std::io::{self, Write};
 
 fn main() {
     let text: String = get_text();
-    let binary = text.as_bytes();
-    let zero_emoji = "⚫";
-    let one_emoji = "🟡";
-
-    for b in binary {
-        let binary_raw = format!("{:08b}", b);
-        let binary_zero = binary_raw.replace("0", zero_emoji);
-        let binary_zero_one = binary_zero.replace("1", one_emoji);
-        println!("{}", binary_zero_one);
-    }
+    println!("{}", get_bulbs(&text));
 }
 
 fn get_text() -> String {
@@ -22,4 +13,19 @@ fn get_text() -> String {
         .read_line(&mut prompt)
         .expect("error: failed to read from stdin");
     prompt.to_string()
+}
+
+fn get_bulbs(text: &str) -> String {
+    let binary = text.as_bytes();
+    let zero_emoji = "⚫";
+    let one_emoji = "🟡";
+    let mut bulbs: String = String::new();
+
+    for b in binary {
+        let binary_raw = format!("{:08b}", b);
+        let binary_zero = binary_raw.replace("0", zero_emoji);
+        let binary_zero_one = binary_zero.replace("1", one_emoji);
+        bulbs = format!("{}\n{}", bulbs, binary_zero_one);
+    }
+    bulbs
 }
